5ca9a56fd51bd4ae6121eeeda4ac166f35989d32
Andrey Vershinin <adw006@ya.ru>
2020-06-09 21:13:13 +0300

    implement welcome screen

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/app/app.jsx';
+
+const ERROR_COUNT = 3;
+
+ReactDOM.render(
+    <App errorCount={ERROR_COUNT} />,
+    document.querySelector(`#root`)
+);
