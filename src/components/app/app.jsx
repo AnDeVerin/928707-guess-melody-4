a836ededc7a0ef8fe9fd7f17ad4ce6b4dcf2f726
Andrey Vershinin <adw006@ya.ru>
2020-06-19 11:49:45 +0300

    setup router & add components

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen.jsx';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen.jsx';
 
 const App = ({ errorCount }) => {
   const onStartHandler = () => {};
+
   return (
-    <WelcomeScreen errorCount={errorCount} onStartHandler={onStartHandler} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen
+            errorCount={errorCount}
+            onStartHandler={onStartHandler}
+          />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
