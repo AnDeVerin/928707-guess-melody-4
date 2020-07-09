a12714c32f8a4fa53b1cfc61345ec6a1ec8afd60
Andrey Vershinin <adw006@ya.ru>
2020-07-09 17:44:52 +0300

    add e2e test for player

--- src/components/audio-player/audio-player.e2e.test.js
+++ src/components/audio-player/audio-player.e2e.test.js
@@ -1,45 +1,22 @@
 import React from 'react';
-import { configure, mount } from 'enzyme';
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 import AudioPlayer from './audio-player.jsx';
 
 configure({ adapter: new Adapter() });
 
-const mock = {
-  song: {
-    src: `https://upload.wikimedia.org/wikipedia/commons/1/1f/Uganda_flag_and_national_anthem_-_Oh_Uganda_Land_o.ogg`,
-  },
-};
+it(`Click by Play button calls callback`, () => {
+  const handlePlayButtonClick = jest.fn();
+  const wrapper = shallow(
+    <AudioPlayer
+      isLoading={false}
+      isPlaying={false}
+      onPlayButtonClick={handlePlayButtonClick}
+    >
+      <audio />
+    </AudioPlayer>
+  );
 
-describe('AudioPlayer', () => {
-  it('should change button class and state on click', () => {
-    const { song } = mock;
-
-    const component = mount(
-      <AudioPlayer
-        isPlaying={true}
-        onPlayButtonClick={jest.fn()}
-        src={song.src}
-      />
-    );
-
-    window.HTMLMediaElement.prototype.play = () => {};
-    window.HTMLMediaElement.prototype.pause = () => {};
-    component.instance().setState({ isLoading: false });
-
-    const playButton = component.find('.track__button');
-    playButton.simulate('click');
-
-    expect(component.instance().state.isPlaying).toEqual(false);
-    expect(
-      component.find(`.track__button`).hasClass('track__button--play')
-    ).toBe(true);
-
-    playButton.simulate('click');
-    expect(component.instance().state.isPlaying).toEqual(true);
-    expect(
-      component.find(`.track__button`).hasClass(`track__button--pause`)
-    ).toBe(true);
-  });
+  wrapper.find(`.track__button`).simulate(`click`);
+  expect(handlePlayButtonClick).toHaveBeenCalledTimes(1);
 });
