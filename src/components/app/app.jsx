7406585a551ef01b5f3e647c05ab29dd3f64e47f
Andrey Vershinin <adw006@ya.ru>
2020-07-14 10:03:28 +0300

    add new reducer to app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,8 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { ActionCreator } from '../../reducer.js';
+import { ActionCreator } from '../../reducer/game/game.js';
+import { AuthorizationStatus } from '../../reducer/user/user.js';
 
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen.jsx';
@@ -12,6 +13,14 @@ import WinScreen from '../win-screen/win-screen.jsx';
 import GameScreen from '../game-screen/game-screen.jsx';
 import withActivePlayer from '../../hocs/with-active-player/with-active-player.js';
 import withUserAnswer from '../../hocs/with-user-answer/with-user-answer.js';
+import {
+  getStep,
+  getMistakes,
+  getMaxMistakes,
+} from '../../reducer/game/selectors.js';
+import { getQuestions } from '../../reducer/data/selectors.js';
+import { getAuthorizationStatus } from '../../reducer/user/selectors.js';
+import { Operation as UserOperation } from '../../reducer/user/user.js';
 
 const GenreQuestionScreenWrapped = withActivePlayer(
   withUserAnswer(GenreQuestionScreen)
@@ -23,6 +32,8 @@ import { GameType } from '../../const.js';
 class App extends PureComponent {
   _renderGameScreen() {
     const {
+      authorizationStatus,
+      login,
       maxMistakes,
       mistakes,
       questions,
@@ -115,6 +126,8 @@ class App extends PureComponent {
 }
 
 App.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+  login: PropTypes.func.isRequired,
   maxMistakes: PropTypes.number.isRequired,
   mistakes: PropTypes.number.isRequired,
   questions: PropTypes.array.isRequired,
@@ -125,13 +138,17 @@ App.propTypes = {
 };
 
 const mapStateToProps = (state) => ({
-  step: state.step,
-  maxMistakes: state.maxMistakes,
-  mistakes: state.mistakes,
-  questions: state.questions,
+  authorizationStatus: getAuthorizationStatus(state),
+  step: getStep(state),
+  maxMistakes: getMaxMistakes(state),
+  questions: getQuestions(state),
+  mistakes: getMistakes(state),
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  login(authData) {
+    dispatch(UserOperation.login(authData));
+  },
   onWelcomeButtonClick() {
     dispatch(ActionCreator.incrementStep());
   },
